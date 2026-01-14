b3dbc83fcdeecf37e2b3b46755abf97c4241f7e1
rezer <antonykj0519@gmail.com>
2026-01-14 10:02:10 +0530

    first commit

---
+++ authpages/script.js
@@ -0,0 +1,9 @@
+document.getElementById("registerForm")?.addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Registered successfully (demo)");
+});
+
+document.getElementById("loginForm")?.addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Login successful (demo)");
+});
